716236260d32e265f5f9381c11509d81bc25c9d5
Rene Trevino <drfrankenstein123@gmail.com>
2023-11-29 00:48:05 -0600

    article routes

--- controllers/api/article-routes.js
+++ controllers/api/article-routes.js
@@ -1,19 +1,20 @@
-const {User, Article, Troll} = require("../../models")
+const { Article } = require("../../models")
 const router = require("express").Router();
 
 
-router.get("/", async (req, res) => {
+router.get("/allArticles", async (req, res) => {
     try {
-        
+       const sadPostData = await Article.findAll();
+       res.status(200).json(sadPostData);
     } catch (err) {
         console.error(err)
-        res.status(500).json(err)
-        
+        res.status(500).json(err)        
     }
 })
 router.post("/", async (req, res) => {
     try {
-        
+     const sadPostData = await Article.create(req.body);
+     res.status(200).json(sadPostData)  
     } catch (err) {
         console.error(err)
         res.status(500).json(err)
@@ -22,7 +23,14 @@ router.post("/", async (req, res) => {
 })
 router.put("/", async (req, res) => {
     try {
-        
+       const sadPostData = await Article.update(req.body, {
+       where:{ name: req.params.name  }
+       }) 
+       if(!sadPostData){
+        res.status(400).json({ message: "no user with that name"})
+       }
+
+       res.status(200).json(sadPostData)
     } catch (err) {
         console.error(err)
         res.status(500).json(err)
@@ -31,11 +39,13 @@ router.put("/", async (req, res) => {
 })
 router.delete("/", async (req, res) => {
     try {
-        
+      const sadPostData = await Article.destroy({
+        where: { name: req.params.name }
+      })  
+      res.status(200).json({sadPostData, message: "article deleted"})
     } catch (err) {
         console.error(err)
-        res.status(500).json(err)
-        
+        res.status(500).json(err)        
     }
 })
 
