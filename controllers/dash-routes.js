b039e8fcabc341439b0619a183d2100154a96302
neyneyalldayday <drfrankenstein123@gmail.com>
2023-12-01 14:25:46 -0600

    updates

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -2,9 +2,21 @@ const router = require("express").Router()
 const {User, Article, Troll} = require("../models");
 const withAuth = require("../utils/auth")
 
-router.get("/", withAuth, async (req,res) => {
+router.get("/",  async (req,res) => {
     try {
-       res.render("profile",{layout: "dash"}) 
+        const profileData = await User.findOne( {   
+            where: {name: req.params.name},
+            include : [Article]          
+        });
+
+        res.status(200).json(profileData)
+
+        const proflieArr = profileData.map((data) => data.get({plain: true}));
+        console.log(proflieArr)
+       res.render("profile",{
+        layout: "dash",
+        proflieArr
+    }) 
     } catch (error) {
         console.error(error)
         res.status(500).json(error)
